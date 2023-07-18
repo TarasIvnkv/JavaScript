7a12e33a9b532e52437f6884f1b72f482c1c396e
TarasIvnkv <tarasivanikov@gmail.com>
2023-07-18 22:46:15 +0300

    Add table and styles for this. Exersize also complete

--- script/script.js
+++ script/script.js
@@ -8,3 +8,40 @@ randomNum = parseFloat(randomNum.toFixed(2));
 alert(`Random number is: ${randomNum}🥳`);
 thirdNum = parseFloat(prompt(`Enter the third fractional number`));
 thirdNum = parseFloat(thirdNum.toFixed(2));
+formulaSum = `${randomNum} + ${thirdNum}`;
+formulaSubtraction = `${randomNum} - ${thirdNum}`;
+formulaMultiplication = `${randomNum} * ${thirdNum}`;
+formulaDivision = `${randomNum} / ${thirdNum}`;
+sum = (randomNum + thirdNum).toFixed(2);
+subtraction = (randomNum - thirdNum).toFixed(2);
+multiplication = (randomNum * thirdNum).toFixed(2);
+division = (randomNum / thirdNum).toFixed(2);
+document.write(`
+    <table>
+        <tr>
+            <th>Operation</th>
+            <th>Expression</th>
+            <th>Value</th>
+        </tr>
+        <tr>
+            <th>Sum</th>
+            <td>${formulaSum}</td>
+            <td>${sum}</td>
+        </tr>
+        <tr>
+            <th>Subtraction</th>
+            <td>${formulaSubtraction}</td>
+            <td>${subtraction}</td>
+        </tr>
+        <tr>
+            <th>Multiplication</th>
+            <td>${formulaMultiplication}</td>
+            <td>${multiplication}</td>
+        </tr>
+        <tr>
+            <th>Division</th>
+            <td>${formulaDivision}</td>
+            <td>${division}</td>
+        </tr>
+    </table>
+`);
